0566444ec9cb072de9d597fb860224b637248991
Matheus Cardoso <matheuscardosooffice@gmail.com>
2023-09-30 19:47:46 -0300

    Teste concluido

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+
+  const [country, setCountry] = useState([])
+
+  const getCountries = async () => {
+    const url = "http://localhost:3001/get-countries";
+
+
+    try {
+      const response = await fetch(url, {
+        method: "POST"
+      });
+
+      const responseText = await response.text();
+      const parser = new DOMParser();
+      const xmlDoc = parser.parseFromString(responseText, "text/xml");
+      console.log(xmlDoc);
+      const countryElements = xmlDoc.getElementsByTagName("m:sName")
+      console.log(countryElements[0].textContent)
+      if (countryElements.length > 0) {
+        setCountry(Array.from(countryElements).map(element => element.textContent))
+      }
+    } catch (error) {
+      console.error("Erro na requisição SOAP", error);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={getCountries}>Obter Lista de Países</button>
+      {country.map(country => (
+        <p>{country}</p>
+      ))}
     </div>
   );
 }
